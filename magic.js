b5f81464b7866792bedb04d873fb1df299ea142a
Yash Kolambekar <41070233+yashkolambekar@users.noreply.github.com>
2021-03-05 21:38:14 +0530

    Add files via upload

---
+++ magic.js
@@ -0,0 +1,14 @@
+function hideheart(){
+    setTimeout(function(){  document.getElementById('heartoverlay').style.opacity='0.9';},100);
+    setTimeout(function(){  document.getElementById('heartoverlay').style.opacity='0.8';},200);
+    setTimeout(function(){  document.getElementById('heartoverlay').style.opacity='0.7';},300);
+    setTimeout(function(){  document.getElementById('heartoverlay').style.opacity='0.6';},400);
+    setTimeout(function(){  document.getElementById('heartoverlay').style.opacity='0.5';},500);
+    setTimeout(function(){  document.getElementById('heartoverlay').style.opacity='0.6';},600);
+    setTimeout(function(){  document.getElementById('heartoverlay').style.opacity='0.5';},700);
+    setTimeout(function(){  document.getElementById('heartoverlay').style.opacity='0.4';},800);
+    setTimeout(function(){  document.getElementById('heartoverlay').style.opacity='0.3';},900);
+    setTimeout(function(){  document.getElementById('heartoverlay').style.opacity='0.2';},1000);
+    setTimeout(function(){  document.getElementById('heartoverlay').style.opacity='0.1';},1100);
+    setTimeout(function(){  document.getElementById('heartoverlay').style.opacity='0';},1200);
+}
